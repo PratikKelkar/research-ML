2b9e940204a1738881403d6dd66f6427850a0180
PratikKelkar <pratikkelkar11@gmail.com>
2017-07-11 12:42:41 -0500

    Update uuhhfixingstuff.py

--- uuhhfixingstuff.py
+++ uuhhfixingstuff.py
@@ -1,50 +1,32 @@
+import scipy.io
 import numpy as np
-import scipy.io as io
+
+mat = scipy.io.loadmat("everything.mat")
+
+tbs = "11 12 23 24 35 36 47 48 59 60 66 67 68"
+tbs = tbs.split(" ")
+
+tools = "11 23 35 47 59 68".split(" ")
 
 
-mat = io.loadmat("everything.mat")
+X = np.zeros((130,2447,650))
+Y = np.zeros((130,))
 
-def init():
-    mat1 = {}
-    for key,val in mat.items():
-        if "W" in key:
-            mat1[key] = val[:,201:851]
+xynum = 0
+for segn in range(1,11):
+    for i in tbs:
+        if i in tools:
+            Y[xynum] = 1    
+        pres_name = "W0" + str(i) + "_Segment_0" + str(segn).zfill(2)
+        pres_val = mat[pres_name]
+        pres_val = pres_val[:,201:851]
+        X[xynum]=pres_val
+        xynum+=1
 
-    avg = np.zeros((2447, 650))
-    for key,val in mat1.items():
-        avg+=val
-    avg/=130
 
-    mat2 = {}
-    for key,val in mat1.items():
-        mat2[key] = val - avg
+np.save("July11x.npy", X)
+np.save("July11y.npy", Y)
 
-    rawvals = list(mat2.values())
-    rawkeys = list(mat2.keys())
 
-    return (rawkeys, rawvals)
-
-q,w = init()
-
-w = np.asarray(w)
-qy = []
-for i in range(130):
-    if "11" in q[i]:
-        qy.append(1)
-    elif "23" in q[i]:
-        qy.append(1)
-    elif "35" in q[i]:
-        qy.append(1)
-    elif "47" in q[i]:
-        qy.append(1)
-    elif "59" in q[i]:
-        qy.append(1)
-    elif "68" in q[i]:
-        qy.append(1)
-    else:
-        qy.append(2)
-
-qy = np.asarray(qy)
-
-np.save("july6x.npy", w)
-np.save("july6y", qy)
+#all 13 words in a row, repeated ten times
+#tools = 1, buildings = 0
