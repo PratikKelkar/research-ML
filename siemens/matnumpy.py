5bcfba2b4f43d299989f8528ab42506fbf42bd15
PratikKelkar <pratikkelkar11@gmail.com>
2017-09-02 17:57:57 -0500

    Add files via upload

--- siemens/matnumpy.py
+++ siemens/matnumpy.py
@@ -22,21 +22,22 @@ def mat_to_numpy():
 
     #create list of X files -- len(5):
 
-    mat_bands = {}
+    mat_bands = []
     
-    mat_bands["deltamat"] = loadmat("delta.mat")
-    mat_bands["thetamat"] = loadmat("theta.mat")
-    mat_bands["alphamat"] = loadmat("alpha.mat")
-    mat_bands["betamat"] = loadmat("beta.mat")
-    mat_bands["gammamat"] = loadmat("gamma.mat")
+    mat_bands.append(loadmat("delta.mat"))
+    mat_bands.append(loadmat("theta.mat"))
+    mat_bands.append(loadmat("alpha.mat"))
+    mat_bands.append(loadmat("beta.mat"))
+    mat_bands.append(loadmat("gamma.mat"))
+    
 
-    for key,val in mat_bands.items():
-        numpy_x = np.zeros((110, 256, 750))      
+    for i in mat_bands:
+        numpy_x = np.zeros((110, 256, 650))      
         c2=0
         for segnum in range(1,11,1):
             for word in allwords:
-                matkey = "W0" + str(allwords).zfill(2) + "_Segment_0" + str(segnum).zfill(2)
-                numpy_x[c2] = val[matkey][:, 101:851]
+                matkey = "W0" + str(word).zfill(2) + "_Segment_0" + str(segnum).zfill(2)
+                numpy_x[c2] = i[matkey][:256, 200:850]
                 c2+=1
 
         returnfilex.append(numpy_x)
